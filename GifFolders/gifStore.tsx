0384a9f8e827980a3f99ead4e8055d69dc1d40e0
LumaSeiran <usagirirain@gmail.com>
2025-07-27 14:48:40 +0300

    Simplified

--- GifFolders/gifStore.tsx
+++ GifFolders/gifStore.tsx
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import { DataStore } from "@api/index";
 import { Logger } from "@utils/Logger";
 import { findByPropsLazy, proxyLazyWebpack } from "@webpack";
 import { FluxDispatcher, RestAPI, UserSettingsActionCreators, UserStore } from "@webpack/common";
@@ -18,7 +17,7 @@ const BINARY_READ_OPTIONS = findByPropsLazy("readerFactory");
 
 let LAST_SAVE_TIME = 0;
 const GIF_TO_BE_UPDATED: { save: Record<string, Gif>; delete: Set<string>; } = { save: {}, delete: new Set() };
-let ALL_GIFS: Record<string, Gif>;
+let ALL_GIFS: Record<string, Gif> | undefined;
 
 export interface Gif {
     url?: string,
@@ -30,60 +29,6 @@ export interface Gif {
     order: number,
 }
 
-async function updateGifs(save?: Record<string, Gif>, del?: string) {
-    const timePassed = Date.now() - LAST_SAVE_TIME;
-
-    console.log("TIME PASSED: ", timePassed);
-    if (timePassed >= 10000) {
-        const key = getKey();
-        if (!key) return;
-
-        const allGifs = await DataStore.get(key);
-        if (!allGifs) return;
-
-        await FrecencyAC.updateAsync(
-            "favoriteGifs",
-            data => {
-                data.gifs = { ...allGifs };
-            },
-            0
-        );
-        console.log("Updated gifs!");
-
-        LAST_SAVE_TIME = Date.now();
-    }
-    else {
-        const waitTime = 10000 - timePassed + 100; // im not sure if the extra time is needed, but i dont want to run into timing issues
-        setTimeout(() => updateGifs(), waitTime);
-    }
-}
-
-async function queueGifUpdate(key?: string, allGifs?: Record<string, Gif>) {
-    if (!allGifs) {
-        console.log("Didn't get the full gif list");
-        return;
-    }
-
-    if (Object.keys(GIF_TO_BE_UPDATED.save).length === 0 && GIF_TO_BE_UPDATED.delete.size === 0) {
-        console.log("No more gifs to change!");
-        return;
-    }
-
-    for (const [url, gif] of Object.entries(GIF_TO_BE_UPDATED.save ?? {})) {
-        allGifs[url] = gif;
-        GIF_TO_BE_UPDATED.save[url] = gif;
-    }
-
-    for (const url of GIF_TO_BE_UPDATED.delete ?? new Set()) {
-        delete allGifs[url];
-        GIF_TO_BE_UPDATED.delete.add(url);
-    }
-
-    console.log("queue running!, updating with the follwing: ", GIF_TO_BE_UPDATED);
-    await DataStore.set(key, allGifs);
-    await updateGifs(allGifs);
-    return allGifs;
-}
 function allLoaded(): boolean {
     try {
         FrecencyAC.ProtoClass;
@@ -113,23 +58,30 @@ function getKey() {
     return `GifFolders:gif:${id}`;
 }
 
+async function updateGifs() {
+    const timePassed = Date.now() - LAST_SAVE_TIME;
 
+    console.log("TIME PASSED: ", timePassed);
+    if (timePassed >= 10000) {
+        await FrecencyAC.updateAsync(
+            "favoriteGifs",
+            data => {
+                data.gifs = { ...ALL_GIFS };
+            },
+            0
+        );
+        console.log("Updated gifs!");
 
-export async function getGif(url: string | undefined) {
-    const key = getKey();
-    if (!key || !url) return undefined;
-
-    const gifs = await DataStore.get(key);
-    return gifs[url];
-}
-async function getAllGifs(key) {
-    const allGifs: Record<string, Gif> | undefined = await getAllFavoritedGifsFromDB(key);
-    if (!allGifs) {
-        new Logger("GifFolders").error("Failed to grab all gifs");
-        return undefined;
+        LAST_SAVE_TIME = Date.now();
     }
-    return allGifs;
+    else {
+        const waitTime = 10000 - timePassed + 100; // im not sure if the extra time is needed, but i dont want to run into timing issues
+        setTimeout(() => updateGifs(), waitTime);
+    }
 }
+
+
+
 
 // change this to get the last free open index instead
 // would it work to Set the values so the editing person can change folders easily?
@@ -139,8 +91,7 @@ export async function handleGifAdd(folder: Folder, gif: Gif, lastVisited: Folder
     const key = getKey();
     if (!key) return;
 
-    const allGifs = await getAllGifs(key);
-    if (!allGifs) return;
+    if (!ALL_GIFS) return;
 
     const { url, ...rest } = gif;
     if (!url) {
@@ -148,14 +99,15 @@ export async function handleGifAdd(folder: Folder, gif: Gif, lastVisited: Folder
         return;
     }
 
-    const highestOrder = Object.values(allGifs)
+    const highestOrder = Object.values(ALL_GIFS)
         .filter(gif => gif.order >= folder.start && gif.order < folder.end)
         .reduce((highest, gif) => highest > gif.order ? highest : gif.order, folder.start - 1);
 
     if (highestOrder + 1 >= folder.end) return; //  should be impossible to reach this
 
-    const newGif = { ...rest, order: highestOrder + 1 };
-    return updateGifs({ save: { [url]: newGif } });
+    ALL_GIFS[url] = { ...rest, order: highestOrder + 1 };
+
+    return updateGifs();
 }
 
 export async function handleGifDelete(gif: Gif, lastVisited: Folder | null = null) {
@@ -169,27 +121,28 @@ export async function handleGifDelete(gif: Gif, lastVisited: Folder | null = nul
     const key = getKey();
     if (!key) return;
 
-    const allGifs = await getAllGifs(key);
-    if (!allGifs) return;
+    if (!ALL_GIFS) return;
 
-    if (!(gif.url in allGifs)) {
+    if (!(gif.url in ALL_GIFS)) {
         new Logger("GifFolders").error("Failed to find the gif, won't delete!");
         return;
     }
+
+    delete ALL_GIFS[gif.url];
 
     return updateGifs();
 }
 
 // Need to use the RestApi because FrecencyAC.getCurrentValue()
 // return the local array of gifs (affected by FluxDispatcher)
-async function getAllFavoritedGifs(): Promise<Record<string, Gif> | null> {
-    if (!allLoaded()) null;
+async function getAllFavoritedGifs(): Promise<Record<string, Gif> | undefined> {
+    if (!allLoaded()) undefined;
     const { ok, status, body } = await RestAPI.get({
         url: "/users/@me/settings-proto/2"
     });
 
     if (!ok || status !== 200 || !body?.settings)
-        return null;
+        return undefined;
 
     const bytes = Uint8Array.from(atob(body.settings), c => c.charCodeAt(0));
     const end = FrecencyAC.ProtoClass.fromBinary(
@@ -198,19 +151,9 @@ async function getAllFavoritedGifs(): Promise<Record<string, Gif> | null> {
     );
 
     if (!end.favoriteGifs || !end.favoriteGifs.gifs)
-        return null;
+        return undefined;
 
     return end.favoriteGifs.gifs;
-}
-
-async function getAllFavoritedGifsFromDB(key: string): Promise<Record<string, Gif> | undefined> {
-    const storedGifs: Record<string, Gif> | undefined = await DataStore.get(key);
-    if (!storedGifs) {
-        console.log("Failed to get the gifs from DB");
-        return;
-    }
-
-    return storedGifs;
 }
 
 function generateProtoFromGifs(gifs: Record<string, Gif>) {
@@ -241,15 +184,14 @@ export async function showSelectedGifs(folder?: Folder | null, gifs?: Record<str
     const key = getKey();
     if (!key) return;
 
-    const allGifs = await getAllGifs(key);
-    if (!allGifs) return;
+    if (!ALL_GIFS) return;
 
     let filteredGifs;
     if (!folder)
-        filteredGifs = allGifs;
+        filteredGifs = ALL_GIFS;
     else {
         filteredGifs = Object.fromEntries(
-            Object.entries(allGifs)
+            Object.entries(ALL_GIFS)
                 .filter(([, { order }]) => order >= folder.start && order < folder.end)
                 .map(([url, data]) => [url, { ...data }])
         );
@@ -274,17 +216,11 @@ export async function initializeGifs() {
     const key = getKey();
     if (!key) return false;
 
-    const allGifs = await getAllFavoritedGifs();
-    if (!allGifs) {
+    ALL_GIFS = await getAllFavoritedGifs();
+    if (!ALL_GIFS) {
         new Logger("GifFolders").error("Failed to grab all gifs");
         return false;
     }
 
-    const storedGifs: Record<string, Gif> | undefined = await DataStore.get(key) ?? {};
-    for (const [url, value] of Object.entries(allGifs)) {
-        storedGifs[url] = value;
-    }
-
-    await DataStore.set(key, storedGifs);
     return true;
 }
